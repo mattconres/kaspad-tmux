0fa3b311eef971c0a51c259d5c4938f19c6583dc
Matthew Williams <mwilliams@conres.com>
2022-08-19 13:13:58 -0400

    Update kaspad-tmux.sh

--- kaspad-tmux.sh
+++ kaspad-tmux.sh
@@ -8,8 +8,10 @@
 
 
 TMUX=$(type -p tmux) || { echo "This script requires tmux"; exit 1; }
-
 SESSION="KASPA-$HOSTNAME"
+
+# kaspa Binary location
+KBIN="#HOME/go/bin"
 
 echo "Attempting to start Session"
 $TMUX -q new-session -d -s "$SESSION" > /dev/null
@@ -19,4 +21,6 @@ then
 	$TMUX -q attach -t "$SESSION" >/dev/null 2>&1
 fi
 
+
+$TMUX -q split-window -t "$SESSION" "printf '\033]2;%s\033\\' 'WalletBalance' ; kaspawallet balance"
 $TMUX -q attach -t "$SESSION" >/dev/null 2>&1
