bbf9b9be3e5f3aa25535de4734442e1c0d8cd1c5
Matthew Williams <mwilliams@conres.com>
2022-08-19 17:35:39 -0400

    Update kaspad-tmux.sh

--- kaspad-tmux.sh
+++ kaspad-tmux.sh
@@ -120,7 +120,7 @@ $TMUX splitw -v -p 10 -t $SESSION:0.0
 $TMUX splitw -h -p 80 -t $SESSION:0.1
 $TMUX send-keys -t $SESSION:0.0 "while true; do ~/go/bin/kaspawallet balance;sleep 3;done" Enter
 $TMUX select-pane -t $SESSION:0.0 -T "Kaspd"
-$TMUX send-keys -t $SESSION:0.0 "printf '\033]2;%s\033\\' 'Kaspd'" Enter
+#$TMUX send-keys -t $SESSION:0.0 "printf '\033]2;%s\033\\' 'Kaspd'" Enter
 $TMUX send-keys -t $SESSION:0.1 "ping 8.8.8.8" Enter
 $TMUX send-keys -t $SESSION:0.2 "while true; do ~/go/bin/kaspawallet balance;sleep 3;done" Enter
 
