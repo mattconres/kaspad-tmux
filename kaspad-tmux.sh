46e206083d4f0d7767f23912b42519c06aa95ce4
Matthew Williams <mwilliams@conres.com>
2022-08-19 13:15:23 -0400

    Update kaspad-tmux.sh

--- kaspad-tmux.sh
+++ kaspad-tmux.sh
@@ -23,4 +23,15 @@ fi
 
 
 $TMUX -q split-window -t "$SESSION" "printf '\033]2;%s\033\\' 'WalletBalance' ; kaspawallet balance"
+$TMUX -q select-layout -t "$SESSION" tiled
+
+
+
+$TMUX set-option -t "$SESSION" -g status-style bg=colour235,fg=yellow,dim
+$TMUX set-window-option -t "$SESSION" -g window-status-style fg=brightblue,bg=colour236,dim
+$TMUX set-window-option -t "$SESSION" -g window-status-current-style fg=brightred,bg=colour236,bright
+
+$TMUX -q set-window-option -t "$SESSION" synchronize-panes on
+$TMUX set-option -t "$SESSION" -w pane-border-status bottom
+
 $TMUX -q attach -t "$SESSION" >/dev/null 2>&1
