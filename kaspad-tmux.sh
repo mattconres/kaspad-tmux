ed9b4a5d980d35a6ad935dd66f025d59912b573d
Matthew Williams <mwilliams@conres.com>
2022-08-19 14:26:39 -0400

    Update kaspad-tmux.sh

--- kaspad-tmux.sh
+++ kaspad-tmux.sh
@@ -31,7 +31,7 @@ $TMUX set-option -t "$SESSION" -q mouse on
 $TMUX rename-window -t $SESSION:0 'Main'
 $TMUX splitw -v -p 10 -t $SESSION:0.0
 $TMUX splitw -h -p 80 -t $SESSION:0.1
-$TMUX send-keys -t $SESSION:0.0 "while true; do ~/go/bin/kaspawallet balance;sleep 3; done" Enter
+$TMUX send-keys -t $SESSION:0.0 "while true; do ~/go/bin/kaspawallet balance;sleep 3; done" 'C-m'
 $TMUX send-keys -t $SESSION:0.1 "ping 8.8.8.8" Enter
 
 # $TMUX new-window -t $SESSION
