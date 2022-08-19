69bf6cd122e0875ab3e498336f8f5e0ef6252146
Matthew Williams <mwilliams@conres.com>
2022-08-19 13:00:31 -0400

    Update kaspad-tmux.sh

--- kaspad-tmux.sh
+++ kaspad-tmux.sh
@@ -12,8 +12,8 @@ TMUX=$(type -p tmux) || { echo "This script requires tmux"; exit 1; }
 SESSION="KASPA-$HOSTNAME"
 
 
-$TMUX -q new-session -d -s "$SESSION"
-
+$TMUX -q new-session -d -s "$SESSION" > /dev/null
+echo "Attempting to start Session"
 if [ $? -eq 1 ]
 then
 	$TMUX -q attach -t "$SESSION" >/dev/null 2>&1
